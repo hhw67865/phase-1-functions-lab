16ceda99525a6bbe45754a955a5263da614b55d7
Henry Wu <hhw67865@gmail.com>
2022-10-11 12:54:35 -0400

    commit tree bce0ba428599ba4a329fd6ebf55cfb3e944d415c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const scuberHQ = 42;
+function distanceFromHqInBlocks(a) {
+    return Math.abs(a-scuberHQ);
+}
+
+function distanceFromHqInFeet(a) {
+    return distanceFromHqInBlocks(a)*264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let price = 0;
+    if (distance > 2500) {
+        return `cannot travel that far`;
+    }
+    if (distance > 2000) {
+        price +=  25;
+    }
+    else if (distance > 400) {
+        price += .02 * (distance - 400);
+    }
+    return price;
+    
+}
